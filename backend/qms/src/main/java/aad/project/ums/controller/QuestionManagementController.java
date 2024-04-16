b510f5e624403556e99be001c1a31b6ce2edf902
JoeHitHard <joseph.meghanath@gmail.com>
2024-04-16 02:07:32 -0500

    qms implementation

---
+++ backend/qms/src/main/java/aad/project/ums/controller/QuestionManagementController.java
@@ -0,0 +1,93 @@
+package aad.project.ums.controller;
+
+import aad.project.qanda.InvalidSessionException;
+import aad.project.qanda.connector.QuestionConnector;
+import aad.project.qanda.entity.Question;
+import aad.project.qanda.entity.User;
+import aad.project.qanda.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/qms")
+public class QuestionManagementController {
+
+    @Autowired
+    private QuestionConnector questionConnector;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/questions")
+    public ResponseEntity<Question> createQuestion(@RequestHeader("Authorization") String authorizationHeader,
+                                                   @RequestBody Question question) throws InvalidSessionException {
+        // Validate user session
+        User user = validateUserSession(authorizationHeader);
+        // Set user ID to the question
+        question.setUser(user);
+        // Save the question
+        Question createdQuestion = questionConnector.save(question);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdQuestion);
+    }
+
+    @GetMapping("/questions")
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        List<Question> questions = questionConnector.findAll();
+        return ResponseEntity.ok(questions);
+    }
+
+    @GetMapping("/questions/{questionId}")
+    public ResponseEntity<Question> getQuestionById(@PathVariable String questionId) {
+        Optional<Question> optionalQuestion = questionConnector.findById(questionId);
+        if (optionalQuestion.isPresent()) {
+            return ResponseEntity.ok(optionalQuestion.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/questions/{questionId}")
+    public ResponseEntity<Question> updateQuestion(@RequestHeader("Authorization") String authorizationHeader,
+                                                   @PathVariable String questionId,
+                                                   @RequestBody Question questionDetails) throws InvalidSessionException {
+        // Validate user session
+        validateUserSession(authorizationHeader);
+        Optional<Question> optionalQuestion = questionConnector.findById(questionId);
+        if (optionalQuestion.isPresent()) {
+            Question existingQuestion = optionalQuestion.get();
+            existingQuestion.setQuestion(questionDetails.getQuestion());
+            Question updatedQuestion = questionConnector.save(existingQuestion);
+            return ResponseEntity.ok(updatedQuestion);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/questions/{questionId}")
+    public ResponseEntity<Void> deleteQuestion(@RequestHeader("Authorization") String authorizationHeader,
+                                               @PathVariable String questionId) throws InvalidSessionException {
+        // Validate user session
+        validateUserSession(authorizationHeader);
+        Optional<Question> optionalQuestion = questionConnector.findById(questionId);
+        if (optionalQuestion.isPresent()) {
+            questionConnector.deleteById(questionId);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Helper method to validate user session
+    private User validateUserSession(String sessionId) throws InvalidSessionException {
+        Optional<User> user = userService.getUser(sessionId);
+        if (user.isEmpty()) {
+            throw new InvalidSessionException("Invalid session");
+        }
+        return user.get();
+    }
+}
---
+++ backend/ums/src/main/java/aad/project/ums/controller/UserManagementController.java
@@ -0,0 +1,42 @@
+package aad.project.ums.controller;
+
+import aad.project.qanda.InvalidSessionException;
+import aad.project.qanda.service.UserService;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/ums")
+public class UserManagementController {
+    private static final Logger LOG = LoggerFactory.getLogger(UserManagementController.class);
+    @Autowired
+    UserService userService;
+
+    @GetMapping("")
+    public ResponseEntity getUserInSession(@RequestHeader("Authorization") @DefaultValue("XXX") String authorizationHeader) throws InvalidSessionException {
+        return ResponseEntity.ok(userService.getUser(authorizationHeader));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity userLogin(@RequestBody String logInData) throws InvalidSessionException {
+        try {
+            JSONObject logInDetails = new JSONObject(logInData);
+            return ResponseEntity.ok(userService.login(logInDetails.getString("username"), logInDetails.getString("password")));
+        } catch (Exception e) {
+            LOG.error("user login Failed, {}", e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    @ExceptionHandler(InvalidSessionException.class)
+    public ResponseEntity<Object> handleInvalidSessionException(InvalidSessionException ex) {
+        LOG.error("user login Failed, {}", ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+}
