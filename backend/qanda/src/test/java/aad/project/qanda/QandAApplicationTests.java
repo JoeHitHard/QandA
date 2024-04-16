24d200f3828f2c3b371216616608612c0cfdc15e
JoeHitHard <joseph.meghanath@gmail.com>
2024-04-16 00:35:11 -0500

    backend init

---
+++ backend/qanda/src/test/java/aad/project/qanda/QandAApplicationTests.java
@@ -0,0 +1,13 @@
+package aad.project.qanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QandAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
